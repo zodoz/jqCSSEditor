a9c0a175ca7cc71683ab1377d876034610570372
Mike Kent <mikerkent@gmail.com>
2011-07-01 17:19:38 +0000

    added bootloader.js

---
+++ bootloader.js
@@ -0,0 +1,27 @@
+(function() {
+	var head = document.getElementsByTagName('head')[0];
+	if(!jQuery) {
+		var jqs = document.createElement("script");
+		jqs.type = "text/javascript";
+		jqs.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
+		head.appendChild(jqs);
+	}
+	if(!jQuery || !jQuery.ui) {
+		var jquis = document.createElement("script");
+		jquis.type = "text/javascript";
+		jquis.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
+		head.appendChild(jquis);
+		var jquicss = document.createElement("LINK");
+		jquicss.type = "text/css";
+		jquicss.rel = "stylesheet";
+		jquicss.rel = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.0/themes/redmond/jquery-ui.css";
+		head.appendChild(jquicss);
+	}
+	var jqCSSEditor = document.createElement("script");
+	jqCSSEditor.type = "text/javascript";
+	jqCSSEditor.src = "https://raw.github.com/zodoz/jqCSSEditor/master";
+	jqCSSEditor.onload = function() {
+		jQuery.cssEditor();
+	};
+	head.appendChild(jqCSSEditor);
+})(this);
