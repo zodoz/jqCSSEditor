8aa90efc2f2255de975e5d9e685cea519ddbf723
Mike Kent <mikerkent@gmail.com>
2011-07-01 17:34:30 +0000

    bootloader loading issue

--- bootloader.js
+++ bootloader.js
@@ -1,27 +1,52 @@
 (function() {
 	var head = document.getElementsByTagName('head')[0];
+	var ldr = window.loader = {
+		toLoad: 0,
+		loaded: 0
+	};
 	if(!window.jQuery) {
 		var jqs = document.createElement("script");
 		jqs.type = "text/javascript";
 		jqs.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
+		ldr.toLoad++;
+		jqs.onload = function() {
+			window.loader.loaded++;
+			loadLibrary();
+		}
 		head.appendChild(jqs);
 	}
 	if(!window.jQuery || !jQuery.ui) {
 		var jquis = document.createElement("script");
 		jquis.type = "text/javascript";
 		jquis.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"
+		ldr.toLoad++;
+		jquis.onload = function() {
+			window.loader.loaded++;
+			loadLibrary();
+		}
 		head.appendChild(jquis);
 		var jquicss = document.createElement("LINK");
 		jquicss.type = "text/css";
 		jquicss.rel = "stylesheet";
 		jquicss.rel = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.0/themes/redmond/jquery-ui.css";
+		ldr.toLoad++;
+		jquicss.onload = function() {
+			window.loader.loaded++;
+			loadLibrary();
+		}
 		head.appendChild(jquicss);
 	}
-	var jqCSSEditor = document.createElement("script");
-	jqCSSEditor.type = "text/javascript";
-	jqCSSEditor.src = "https://raw.github.com/zodoz/jqCSSEditor/master";
-	jqCSSEditor.onload = function() {
-		jQuery.cssEditor();
-	};
-	head.appendChild(jqCSSEditor);
+
+	function loadLibrary() {
+		console.log("toLoad: ",ldr.toLoad,", loaded:",ldr.loaded);
+		if(ldr.toLoad != ldr.loaded)
+			return;
+		var jqCSSEditor = document.createElement("script");
+		jqCSSEditor.type = "text/javascript";
+		jqCSSEditor.src = "https://raw.github.com/zodoz/jqCSSEditor/master";
+		jqCSSEditor.onload = function() {
+			jQuery.cssEditor();
+		};
+		head.appendChild(jqCSSEditor);
+	}
 })(this);
