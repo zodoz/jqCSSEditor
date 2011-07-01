3651ef90d36463b940cabb297ace3deb03d6abad
Mike Kent <mikerkent@gmail.com>
2011-07-01 17:26:49 +0000

    now with bootloader

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+(function() {
+	var bootloader = document.createElement("script");
+	bootloader.type = "text/javascript";
+	bootloader.src = "https://raw.github.com/zodoz/jqCSSEditor/master/bootloader.js"
+	document.getElementsByTagName('head')[0].appendChild(bootloader);
+})();
