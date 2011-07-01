6d7ea7e5330f7561939f38431ed6cbaadb433ae5
Mike Kent <mikerkent@gmail.com>
2011-07-01 16:44:54 +0000

    basic element

--- jqCSSEditor.js
+++ jqCSSEditor.js
@@ -1,9 +1,25 @@
 (function($) {
+	var loaded = false, el;
+
   /**
  	* main function.
  	* gets the current editor or creates one.
  	*/
   $.cssEditor = function() {
-		console.log("loaded!!!");
+		if(!loaded) {
+			buildCSSEditor();
+			loaded = true;
+		}
   };
+
+	function buildCSSEditor() {
+		el = $('<div>',
+				{
+					id: 'jqCSSEditor',
+					html: "drag me!"
+				}
+			)
+			.draggable()
+			.appendTo($('body'));
+	}
 })(jQuery);
